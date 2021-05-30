a0127a22aa79bdfd988ba8f502c0009a5a419800
LccccccIce <Lcccccc1118@gmail.com>
2021-05-30 10:27:34 +0800

    学习操作系统进程

--- 408/DS/test.c
+++ 408/DS/test.c
@@ -1,23 +1,29 @@
 #include "stdio.h"
 #include<stdlib.h>
+#include"malloc.h"
 typedef struct link
 {
     int *data;
+    struct link *next;
 }link;
 
-
-void test(link* l)
+void creatlink(struct link *l)
 {
     l=(link*)malloc(sizeof(link));
-    l->data=0;
-    printf("%d\n",l);
+    link *s;
+    for (int i = 0; i < 10; i++)
+    {
+        s=(link*)malloc(sizeof(link))
+        s->data=i;
+        s->next=l->next;
+        l->next=s;
+        printf(s->data);
+    }
+    
+    //return l;
 }
 
-int main()
+void main()
 {
-    link *l;
-    test(l);
-    printf("%d\n",l);
-
-    return 0;
+    creatlink(l);
 }
