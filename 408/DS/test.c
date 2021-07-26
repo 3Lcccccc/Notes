700833f09dadc0c90b43483e3c450d7b5fb93b5c
LccccccIce <Lcccccc1118@gmail.com>
2021-07-26 20:41:32 +0800

    修正

--- 408/DS/test.c
+++ 408/DS/test.c
@@ -1,25 +1,16 @@
 #include"stdio.h"
 
-int sum(int begin,int end)
+int sum(int *begin,int *end)
 {
     int sum;
-    printf("%d\n",sum);
-    printf("%d\n",&sum);
-    for (int i = begin; i < end; i++)
-    {   
-        sum+=i;
-    }
-    printf("%d ",sum);
-    return sum;
+    *begin=10;    
+    return 0;
 }
 
 int main ()
 {
-    int *p=0x4015b0;
-    printf("%d",p);
-    sum(1,10);
-    sum(10,20);
-    sum(20,60);
-    
+    int *a=0,b=5;
+    sum(&a,&b);
+    printf("%d",a);
     return 0;
 }
