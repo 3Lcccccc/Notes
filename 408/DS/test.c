91ec2165e1c9c7e9e2b56d5b2595ce91a15516f0
LccccccIce <Lcccccc1118@gmail.com>
2021-08-11 23:35:07 +0800

    测试

--- 408/DS/test.c
+++ 408/DS/test.c
@@ -1,12 +1,12 @@
 #include "stdio.h"
-#include "math.h"
+#include "math.h"
 
-void main()
+void main()
 {
-    double x,s;
-    printf("input number:\n");
-    scanf("%lf",&x);
-    s=sin(x);
-    printf("sin of %lf is %lf\n",x,s);
-
+    double x,s;
+    printf("input number:\n");
+    scanf("%lf",&x);
+    s=sin(x);
+    printf("sin of %lf is %lf\n",x,s);
+    
 }
