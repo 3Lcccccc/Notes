388674e18f4fd8cc5feb0246f1d461bc50faec1a
LccccccIce <Lcccccc1118@gmail.com>
2021-08-19 14:51:00 +0800

    数据结构

---
+++ 408/DS/tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%d\n",l->data);
+    //     l=l->next;
+    // }
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%d\n",l->data);
+    //     l=l->next;
+    // }
--- 408/DS/test.c
+++ 408/DS/test.c
@@ -1,12 +1,60 @@
+#include "stdlib.h"
 #include "stdio.h"
-#include "math.h"
 
-void main()
+typedef struct Link
 {
-    double x,s;
-    printf("input number:\n");
-    scanf("%lf",&x);
-    s=sin(x);
-    printf("sin of %lf is %lf\n",x,s);
+    struct Link * next;
+    int * data;
+}Link, *node;
+struct  Link *create(struct Link *l)
+{
+    struct Link *l1=(struct Link*)malloc(sizeof(struct Link));
+    struct Link *l2;
+    l1->data=0;
+    l=l1;
+    for (int i=1; i < 10; i++)
+    {
+        l2=(struct Link*)malloc(sizeof(struct Link));
+        l2->data=i;
+        l1->next=l2;
+        l1=l2;
+    }
+    return l;
+}
+
+struct  Link *Del(struct  Link *l,int *x)
+{
+    if (l==NULL)
+    {
+        return NULL;
+    }
+    l->next=Del(l->next,x);
+    printf("%d\n",l->data);
+    if (l->data==x)
+    {
+        return l->next;
+    }
+    else 
+    {
+        return l;
+    }   
+}
+
+int main(int argc, char const *argv[])
+{
+    struct Link *l=(struct Link*)malloc(sizeof(struct Link));
+    l=create(l);
+    l=Del(l,5);
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%d\n",l->data);
+    //     l=l->next;
+    // }
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%d\n",l->data);
+    //     l=l->next;
+    // }
     
+    return 0;
 }
